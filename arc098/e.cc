4941aecd2ebb5bc4f387b05183f0fc9767de3a94
iwannatto <iwannatto@gmail.com>
2018-07-26 21:36:57 +0900

    arc098 e

--- arc098/e.cc
+++ arc098/e.cc
@@ -20,19 +20,32 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> Pair;
 
-// const int kMaxN;
+const int kMaxN = 2000;
 
-int N;
-int ans = 0;
+int N, K, Q;
+int A[kMaxN];
+int ans = 1000000000;
 
 signed main() {
-  cin >> N;
+  cin >> N >> K >> Q;
+  REP(i, N) { cin >> A[i]; }
 
-  1 5283679 4
-  12 583679 5
-  123 58679 7
-  1235 8679 7
-  12356 879 8
+  REP(i, N) {
+    int Y = A[i];
+
+    vector<int> v;
+    int j = 0;
+    while (j < N) {
+      vector<int> v2;
+      while (j < N && A[j] >= Y) { v2.push_back(A[j]); ++j; }
+      sort(v2.begin(), v2.end());
+      REP(k, (int)v2.size() - K + 1) { v.push_back(v2[k]); }
+      ++j;
+    }
+
+    sort(v.begin(), v.end());
+    if (v.size() >= Q) { ans = min(ans, v[Q-1]-Y); }
+  }
 
   cout << ans << endl;
   return 0;
