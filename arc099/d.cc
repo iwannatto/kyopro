d9683d3c6f0cf0033bf479b6c3cef6d776ec5887
iwannatto <iwannatto@gmail.com>
2018-08-12 22:05:06 +0900

    arc099

--- arc099/d.cc
+++ arc099/d.cc
@@ -25,16 +25,37 @@ typedef pair<int, int> Pair;
 int K;
 int ans = 0;
 
+int S(int n) {
+  int s = 0;
+  while (n != 0) {
+    s += n%10;
+    n /= 10;
+  }
+  return s;
+}
+
+int F(int n) {
+  int fn, base = 1;
+  double ratio = 1000000000000000;
+  while (base <= n*10) {
+    int x = (n/base + 1)*base - 1;
+    double nr = (double)x/S(x);
+    if (nr < ratio) {
+      ratio = nr;
+      fn = x;
+    }
+    base *= 10;
+  }
+  return fn;
+}
+
 signed main() {
   cin >> K;
 
-  REP(i, 9) {
-    if (K-- > 0) { cout << i+1 << endl; }
-  }
-  int i = 19;
-  while (K-- > 0) {
-    cout << i << endl;
-    i = i*10 + 9;
+  int n = 0;
+  while (K--) {
+    n = F(n+1);
+    cout << n << endl;
   }
   return 0;
 }
