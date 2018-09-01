73d9ef3514d33450a2a87e5f40dfd11fde172f00
iwannatto <iwannatto@gmail.com>
2018-09-01 15:44:47 +0900

    soundhound2018-summer-qual_d

--- library/adjacency_list_graph.cc
+++ library/adjacency_list_graph.cc
@@ -1,7 +1,11 @@
 // AdjacencyListGraph requirements
 #define REP(i, n) for (int i = 0; i < n; ++i)
+#define NINTH_POWER_OF_TEN 1000000000
+#include <queue>
 #include <vector>
 using namespace std;
+typedef pair<int, int> Pair;
+
 
 // AdjacencyListGraph begin
 
@@ -15,7 +19,9 @@ class AdjacencyListGraph {
 
   int n;
   vector<vector<Edge>> es;
-  AdjacencyListGraph(int n) : n(n), es(n) {}
+  vector<int> ds;
+  AdjacencyListGraph(int n) :
+  n(n), es(n), ds(n, NINTH_POWER_OF_TEN) {}
 
   void AddEdge(int from, int to, int cost) {
     Edge e(to, cost);
@@ -32,6 +38,27 @@ class AdjacencyListGraph {
     }
     // something
     return 0;
+  }
+
+  void Dijkstra(int s) {
+    ds[s] = 0;
+    priority_queue<Pair, vector<Pair>, greater<Pair>> q;
+
+    q.push(Pair(0, s));
+    while (!q.empty()) {
+      Pair p = q.top(); q.pop();
+      int d = p.first, v = p.second;
+      if (ds[v] < d) { continue; }
+
+      REP(i, es[v].size()) {
+        Edge e = es[v][i];
+        int nd = ds[v] + e.cost;
+        if (nd < ds[e.to]) {
+          ds[e.to] = nd;
+          q.push(Pair(nd, e.to));
+        }
+      }
+    }
   }
 };
 
