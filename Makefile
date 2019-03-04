6d77c262827e0e1736b82a1cd57681483718d4d3
iwannatto <iwannatto@gmail.com>
2019-03-04 10:48:13 +0900

    abc117_d

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 T = template/t.cc
 M = template/memo.txt
 
-yahoo-procon2019-qual :
+abc117 :
 	mkdir $@
 	# cp ${T} $@/a.cc
 	# cp ${T} $@/b.cc
-	cp ${T} $@/c.cc
-	# cp ${T} $@/d.cc
+	# cp ${T} $@/c.cc
+	cp ${T} $@/d.cc
 	# cp ${T} $@/e.cc
 	# cp ${T} $@/f.cc
 	cp ${M} $@/
--- abc117/d.cc
+++ abc117/d.cc
@@ -1,3 +1,5 @@
+// header
+
 #include <algorithm>
 #include <bitset>
 #include <cmath>
@@ -27,18 +29,51 @@ using namespace std;
 
 typedef pair<int, int> Pair;
 
-// ---
+// header
 
+// code
+
+const int Nlim = POW_10_5;
+int N, K, A[Nlim];
 
 int Ans() {
+  int cnt[40] = {};
+  REP(i, 40) {
+    REP(j, N) {
+      cnt[i] += (A[j] >> i) & 1;
+    }
+  }
 
+  int ans = 0;
+
+  int ans2 = 0;
+  REP(i, 40) {
+    if (((K + 1) >> i) & 1) {
+      ans2 += (N - cnt[i]) * (1ll << i);
+    } else {
+      ans2 += cnt[i] * (1ll << i);
+    }
+  }
+
+  REP(i, 40) {
+    if (((K + 1) >> i) & 1) {
+      ans2 = ans2 - ((N - cnt[i]) * (1ll << i)) + (cnt[i] * (1ll << i));
+      ans = MAX(ans, ans2);
+    }
+    ans2 = ans2 - (cnt[i] * (1ll << i)) + (MAX(N - cnt[i], cnt[i]) * (1ll << i));
+  }
+
+  return ans;
 }
 
 signed main() {
-  cin >>
+  cin >> N >> K;
+  REP(i, N) { cin >> A[i]; }
 
   auto ans = Ans();
 
   cout << ans << endl;
   return 0;
 }
+
+// code
