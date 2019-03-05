2dcb0298f60d658977e75265b56ff7eb42ea0b9a
iwannatto <iwannatto@gmail.com>
2019-03-05 23:58:26 +0900

    agc024_ab memo

--- agc024/a.cc
+++ agc024/a.cc
@@ -2,25 +2,19 @@
 #include <bitset>
 #include <cmath>
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <map>
 #include <queue>
 #include <set>
 #include <string>
 #include <utility>
-#include <unordered_map>
-#include <unordered_set>
 using namespace std;
 
 #define ABS(a) (((a) >= 0) ? (a) : -(a))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) > (b) ? (b) : (a))
-#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
-#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
-#define REP(i, n) for (int i = 0; i < (n); ++i)
-#define NINTH_POWER_OF_TEN 1000000000
-#define LL_INF (numeric_limits<long long>::max()/2)
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i > b; --i)
+#define REP(i, n) for (int i = 0; i < n; ++i)
 #define int long long
 
 typedef long long ll;
@@ -28,13 +22,17 @@ typedef pair<int, int> Pair;
 
 // const int kMaxN;
 
-int N;
+int A, B, C, K;
 int ans = 0;
 
 signed main() {
-  cin >> N;
+  cin >> A >> B >> C >> K;
 
-
+  if (K%2 == 0) {
+    ans = A-B;
+  } else {
+    ans = B-A;
+  }
 
   cout << ans << endl;
   return 0;
