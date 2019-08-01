3143db9917a05193ad36931d7d4e6cb628f6646d
iwannatto <iwannatto@gmail.com>
2019-08-01 15:58:30 +0900

    Gakka PC clean

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 T = template/t.cc
 M = template/memo.txt
 
-abc108 :
+kicstart2019b :
 	mkdir $@
-	# cp ${T} $@/a.cc
-	# cp ${T} $@/b.cc
-	# cp ${T} $@/c.cc
+	cp ${T} $@/a.cc
+	cp ${T} $@/b.cc
+	cp ${T} $@/c.cc
 	# cp ${T} $@/d.cc
 	# cp ${T} $@/e.cc
 	# cp ${T} $@/f.cc
---
+++ kicstart2019b/b.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#define ABS(a) (((a) >= 0) ? (a) : -(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define POW_10_5 100000
+#define POW_10_9 1000000000
+#define MOD 1000000007
+#define INF (numeric_limits<int>::max()/2)
+#define int long long
+
+typedef pair<int, int> Pair;
+typedef unordered_map<int, int> Hash;
+
+// ---
+
+signed main() {
+  int T;
+  cin >> T;
+
+  FOR(caseno, 1, T) {
+    int N;
+    cin >> N;
+    vector<int> S(N), E(N), L(N);
+    REP(i, N) { cin >> S[i] >> E[i] >> L[i]; }
+
+    
+  }
+
+  return 0;
+}
